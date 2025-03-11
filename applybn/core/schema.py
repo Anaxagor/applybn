b60c62a07303ec9fbb3ba30c0decb9fbceb2b83c
Roman Netrogolov <romius2001@gmail.com>
2025-03-11 13:46:11 +0300

    Core update: BNEstimators and Pipelines

--- applybn/core/schema.py
+++ applybn/core/schema.py
@@ -1,7 +1,8 @@
-from typing import TypedDict, Optional, Sequence, Tuple, List, Union, Callable, TypeVar
+from typing import TypedDict, Optional, Sequence, Tuple, List, Union, Callable, TypeVar, Literal
 from applybn.anomaly_detection.scores.score import Score
 
 
+# bamt inner parameters
 class StructureLearnerParams(TypedDict, total=False):
     init_edges: Optional[Sequence[str]]
     init_nodes: Optional[List[str]]
@@ -9,7 +10,7 @@ class StructureLearnerParams(TypedDict, total=False):
     white_list: Optional[Tuple[str, str]]
     bl_add: Optional[List[str]]
 
-
+# parameters for bamt
 class ParamDict(TypedDict, total=False):
     scoring_function: Union[Tuple[str, Callable], Tuple[str]]
     progress_bar: bool
@@ -17,6 +18,14 @@ class ParamDict(TypedDict, total=False):
     regressor: Optional[object]
     params: Optional[StructureLearnerParams]
     optimizer: str
+
+# parameters for BNEstimator
+class BNEstimatorParams(TypedDict, total=False):
+    has_logit: bool
+    use_mixture: bool
+    bn_type: Optional[str]
+    partial: Union[False, Literal["parameters", "structure"]]
+    learning_params: Optional[ParamDict]
 
 
 scores = TypeVar("scores", bound=Score)
